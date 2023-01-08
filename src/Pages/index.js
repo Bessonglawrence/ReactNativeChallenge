bd055ac09a57d11de97237adc647bec8691fc3a8
Base <orockbessonglawrence@gmail.com>
2023-01-08 18:11:37 +0000

    Solved icon not showing problem and routed screens on the bottom tab

---
+++ src/Pages/index.js
@@ -0,0 +1,13 @@
+import SettingScreen from "./SettingScreen/SettingScreen";
+import HomeScreen from "./HomeScreen/HomeScreen";
+import LoginScreen from "./LoginScreen/LoginScreen";
+import RegisterScreen from "./RegisterScreen/RegisterScreen";
+import ProfileScreen from "./ProfileScreen/ProfileScreen"
+
+export {
+    SettingScreen,
+    HomeScreen,
+    LoginScreen,
+    RegisterScreen,
+    ProfileScreen
+}
---
+++ src/Routes/Navigatiion.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { 
+    HomeScreen, 
+    ProfileScreen, 
+    RegisterScreen, 
+    SettingScreen, 
+} from '../Pages';
+
+
+const Tab = createBottomTabNavigator();
+
+const TabNavigation = () =>{
+    return(
+        <NavigationContainer independent={true}>
+            <Tab.Navigator
+                    screenOptions={({ route }) => ({
+                    tabBarIcon: ({ focused, color, size }) => {
+                        let iconName;
+            
+                        if (route.name === 'Home') {
+                            iconName = focused
+                                ? 'home'
+                                : 'home-outline';
+                        } else if (route.name === 'Profile') {
+                            iconName = focused 
+                                ? 'person-outline' 
+                                : 'person-circle-outline';
+                        } else if ( route.name === 'Setting'){
+                            iconName = focused
+                                ? 'settings'
+                                : 'settings-outline';
+                        } else if ( route.name === 'Register'){
+                            iconName = focused 
+                                ? 'list-outline'
+                                : 'list-circle-outline';
+                        }
+            
+                        // You can return any component that you like here!
+                        return <Ionicons name={iconName} size={35} color={color} />;
+                    },
+                    tabBarActiveTintColor: '#3798c8',
+                    tabBarInactiveTintColor: 'gray',
+                    tabBarLabelStyle:{
+                        fontSize: 13,
+                        paddingBottom: 2
+                    },
+                    tabBarStyle:{
+                        height: 55
+                    }
+                    })}
+            >
+                <Tab.Screen name='Home' component={HomeScreen} options={{headerShown: false}}/>
+                <Tab.Screen name='Profile' component={ProfileScreen} />
+                <Tab.Screen name='Setting' component={SettingScreen} />
+                <Tab.Screen name='Register' component={RegisterScreen} />
+            </Tab.Navigator>
+        </NavigationContainer>
+    )
+}
+
+export default TabNavigation;
